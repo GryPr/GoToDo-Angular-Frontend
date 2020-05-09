d8c689c081f3d21f3a1cdb2f14d5e94b73aed363
Kevin <promkev@gmail.com>
2020-05-09 18:26:45 -0400

    Add task interface

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task'
 
 @Component({
   selector: 'app-tasks',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+
+  tasks: Task[];
 
   constructor() { }
 
