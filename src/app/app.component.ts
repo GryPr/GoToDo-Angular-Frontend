ed5c11fa3da30d0911df7874c16dabe1ac15c2f8
Kevin <promkev@gmail.com>
2020-06-01 23:40:37 -0400

    Remove oAuth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import { OAuthService, JwksValidationHandler, AuthConfig } from 'angular-oauth2-oidc'
-
-export const authCodeFlowConfig: AuthConfig = {
-  issuer: '',
-  redirectUri: window.location.origin,
-  clientId: ''
-}
 
 @Component({
   selector: 'app-root',
@@ -15,9 +8,6 @@ export const authCodeFlowConfig: AuthConfig = {
 export class AppComponent {
   title = 'GoToDo Angular Frontend';
 
-  constructor(private OAuthService: OAuthService){
-    this.OAuthService.configure(authCodeFlowConfig);
-    this.OAuthService.tokenValidationHandler = new JwksValidationHandler();
-    this.OAuthService.loadDiscoveryDocumentAndTryLogin();
+  constructor(){
   }
 }
